95fa9068c20a4fc94010785da48bb3c6f3716fa7
lilin <118693994+lilinlim@users.noreply.github.com>
2026-01-15 14:36:09 -0500

    task 1 start

--- task1/question1/js/script.js
+++ task1/question1/js/script.js
@@ -1,10 +1,12 @@
 "use strict";
 
 function setup() {
-    console.log("go")
-
+    createCanvas(100, 100);
+    background(200);
 }
 
 function draw() {
-
+    noStroke();
+    ellipse(20,20, 20, 20);
+    fill(255,192,203);
 }
